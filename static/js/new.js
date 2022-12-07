754f5622ba4f7533c3897872edff9b8c172f3702
AISHWARYA WAGH <waghaishwarya8@gmail.com>
2022-12-07 18:50:41 +0530

    first commit

---
+++ static/js/new.js
@@ -0,0 +1,35 @@
+function click1() {
+    let text1=document.querySelector('#content1');
+    let cardBody1=document.querySelector('#card1');
+    text1.innerText="Diyas are God’s way of telling you that there will always be a light to drive away darkness…May the light of diyas spread joy to your life..🙂"
+    cardBody1.style.backgroundColor="purple";
+    cardBody1.style.color="white";
+    text1.style.padding="10px"
+    text1.style.color="white";
+    text1.style.textAlign="justify"
+    text1.style.boxShadow="0 0 15px white"
+}
+function click2() {
+    let text2=document.querySelector('#content2');
+    let cardBody2=document.querySelector('#card2');
+    text2.innerText="Life is the most beautiful festival, enjoy all the days with full of happiness and colors."
+    cardBody2.style.backgroundColor="rgb(0,50,0)";
+    cardBody2.style.color="white";
+    text2.style.padding="10px"
+    text2.style.color="white";
+    text2.style.textAlign="justify"
+    text2.style.boxShadow="0 0 15px white"
+
+}
+function click3() {
+    let text3=document.querySelector('#content3');
+    let cardBody2=document.querySelector('#card3');
+    text3.innerText="May the truth always win and good triumph over evil. May the Lord always bless you with wisdom and good health."
+    cardBody2.style.backgroundColor="rgb(0,0,50)";
+    cardBody2.style.color="white";
+    text3.style.padding="10px"
+    text3.style.color="white";
+    text3.style.textAlign="justify"
+    text3.style.boxShadow="0 0 15px white"
+
+}
